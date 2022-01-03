c1a8772ae891a67f05ed332fab4daeea90e25dd4
t3rmit4nk <official.simone.rizzo@gmail.com>
2022-01-03 13:33:16 +0100

    tring test on different load balance task

--- python/twoThreadsClaim.py
+++ python/twoThreadsClaim.py
@@ -15,35 +15,53 @@ def bench(n_threads=1, seq_iter=1, iter=1):
     def bench_inner(func):
         def wrapper(*args, **kwargs):
             def thread(index):
+                print("started thread: "+str(index))
                 for i in range(seq_iter):
                     func(*args, **kwargs)
                 print("ended thread n:"+str(index))
 
             exec_times = []
             for n in range(iter):
-                threads_list = []                       # take the start time
-                start_time = time.time()
-                for i in range(n_threads):              #spawning thread
+                threads_list = []
+                start_time = time.perf_counter()               # take the start time
+                for i in range(n_threads):              # spawning thread
                     t = Thread(target=thread, args=(i,))
                     threads_list.append(t)
                     t.start()
-                for t in threads_list:                  #joining all thread
+                for t in threads_list:                  # joining all thread
                     t.join()
-                end_time = time.time() - start_time
+                end_time = time.perf_counter() - start_time
                 exec_times.append(end_time)             # take the end time
 
             res_dict = {'fun': func.__name__, 'args': args, 'n_threads': n_threads,
-                        'seq_iter': seq_iter, 'iter': iter, 'mean':statistics.mean(exec_times),
-                        'variance':statistics.variance(exec_times, statistics.mean(exec_times))}
+                        'seq_iter': seq_iter, 'iter': iter, 'mean': statistics.mean(exec_times),
+                        'variance': 1}#statistics.variance(exec_times, statistics.mean(exec_times))}
             return res_dict
         return wrapper
     return bench_inner
 
 
-@bench(n_threads=6, seq_iter=1000, iter=10)
 def somma(a, b):
+    time.sleep(0.001)
     return a + b
 
+
+def just_wait(n): # NOOP for n/10 seconds
+    time.sleep(n * 0.1)
+
+
+def grezzo(n): # CPU intensive
+    for i in range(2**n):
+        pass
+
+
 # res = bench()(somma)(3,5)
-res = somma(1,2)
+res = bench(n_threads=1, seq_iter=16, iter=1)(grezzo)(20)
 print(res)
+res = bench(n_threads=2, seq_iter=8, iter=1)(grezzo)(20)
+print(res)
+res = bench(n_threads=4, seq_iter=4, iter=1)(grezzo)(20)
+print(res)
+res = bench(n_threads=8, seq_iter=2, iter=1)(grezzo)(20)
+print(res)
+
