c433e00e658e1895e8fff0f41ffd4eec5895f8d3
t3rmit4nk <official.simone.rizzo@gmail.com>
2022-01-15 10:52:14 +0100

    First commit of TIC-TAC TOE GAME

---
+++ first_assignment/exercise1/tictactoe/src/main/java/com/rizzo/tictactoe/TTTController.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rizzo.tictactoe;
+import javax.swing.JLabel;
+/**
+ *
+ * @author Simone
+ */
+public class TTTController extends JLabel {
+    
+    public TTTController() {
+        setText("SONO STATO INIZIALIZZATO");
+    }
+    
+}
